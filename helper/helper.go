b19791272c0ad532ba40209e296be7efcbdaa35c
“Agus <agus40994@gmail.com>
2024-02-25 21:25:08 +0700

    Add Function Input Helper

--- helper/helper.go
+++ helper/helper.go
@@ -2,6 +2,14 @@ package helper
 
 import "fmt"
 
+// function for take input
+func InputHelper(message string) string{
+	fmt.Print(message)
+	var result string;
+	fmt.Scanln(&result);
+	return result 
+}
+
 func Line(){
 	fmt.Println("----------------------------")
 }
